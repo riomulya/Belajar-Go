03517b23e5da7c48f1d45977b9194d781baab250
RioMulya <riomulya75@gmail.com>
2022-04-29 14:06:46 +0700

    update api

--- api/api.go
+++ api/api.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
+	_ "mysql-master"
 	"net/http"
 )
 
@@ -12,8 +15,20 @@ type pelajar struct {
 	umur  int
 }
 
-func main() {
+var data []pelajar
 
+func koneksi() (*sql.DB, error) {
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/golang_sql")
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func main() {
+	sql_tampil()
+	http.HandleFunc("/data", ambilData)
+	http.HandleFunc("/cariData", cariData)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "<h1>Halaman Utama</h1>")
 	})
@@ -27,5 +42,68 @@ func index(w http.ResponseWriter, r *http.Request) {
 }
 
 func ambilData(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 
+	if r.Method == "POST" {
+		result, err := json.Marshal(data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		w.Write(result)
+		return
+	}
+	http.Error(w, "", http.StatusBadRequest)
+}
+
+func cariData(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method == "POST" {
+		namaData := r.FormValue("nama")
+		for _, d := range data {
+			if d.nama == namaData {
+				result, err := json.Marshal(d)
+
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+				}
+				w.Write(result)
+				return
+			}
+		}
+		http.Error(w, "Data Pelajar Tidak Tersedia", http.StatusBadRequest)
+		return
+	}
+	http.Error(w, "", http.StatusBadRequest)
+}
+
+func sql_tampil() {
+	db, err := koneksi()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer db.Close()
+
+	rows, err := db.Query("select * from tbl_data")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		p := pelajar{}
+		err := rows.Scan(&p.id, &p.nama, &p.kelas, &p.umur)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		data = append(data, p)
+	}
+	if err = rows.Err(); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 }
---
+++ api/http-request.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+var baseUrl = "http://localhost:8080"
+
+type pelajar struct {
+	id    int
+	nama  string
+	kelas int
+	umur  int
+}
+
+func ambilApi() ([]pelajar, error) {
+	var err error
+	var client = &http.Client{}
+	var data []pelajar
+
+	request, err := http.NewRequest("POST", baseUrl+"/data", nil)
+
+	if err != nil {
+		return nil, err
+	}
+	response, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+	err = json.NewDecoder(response.Body).Decode(&data)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func main() {
+	var datapel, err = ambilApi()
+	if err != nil {
+		fmt.Println("Error!", err.Error())
+		return
+	}
+	for _, each := range datapel {
+		fmt.Println("ID : ", each.id, "NAMA : ", each.nama, "KELAS : ", each.kelas, "UMUR : ", each.umur)
+	}
+}
